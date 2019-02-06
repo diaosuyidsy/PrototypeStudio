29223c3ad070163b6f7fcd79d6605af320eecf0d
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-02-06 15:45:26 -0500

    adde

--- Assets/Scripts/Week2/GameManager.cs
+++ Assets/Scripts/Week2/GameManager.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Week2
 {
+    [RequireComponent(typeof(AudioSource))]
     public class GameManager : MonoBehaviour
     {
         public static GameManager GM;
+        public AudioClip[] ChipEatingClips;
+        public TextAsset TextScript;
+        public Text ScripterText;
+
+        [HideInInspector]
+        public State GameState;
+
+        private int _clipPointer;
+        private int _scriptPointer;
+        private AudioSource _as;
+        private string[] _scriptTexts;
 
         private void Awake()
         {
             GM = this;
+            GameState = State.Start;
+            _as = GetComponent<AudioSource>();
+            _initializeTextScript();
+
+        }
+
+        private void _initializeTextScript()
+        {
+            _scriptTexts = TextScript.text.Split('\n');
         }
 
         // Update is called once per frame
@@ -19,13 +41,42 @@ namespace Week2
             _checkInput();
         }
 
+        /// <summary>
+        /// Checks the input.
+        /// </summary>
         private void _checkInput()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (GameState != State.Start) return;
+            if (Input.GetMouseButtonDown(0) && !_as.isPlaying)
             {
-
+                _as.clip = ChipEatingClips[_clipPointer];
+                _as.Play();
+                _clipPointer = (_clipPointer + 1) % ChipEatingClips.Length;
+                _progressText();
             }
         }
+
+        /// <summary>
+        /// Progresses the text by 1 at a time
+        /// </summary>
+        private void _progressText()
+        {
+            if (_scriptPointer < _scriptTexts.Length)
+            {
+                ScripterText.text = _scriptTexts[_scriptPointer++];
+            }
+            else
+            {
+                ScripterText.text = "Game End";
+            }
+        }
+    }
+
+    public enum State
+    {
+        PreStart,
+        Start,
+        End
     }
 }
 
