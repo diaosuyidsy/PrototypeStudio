3e92a051ad6f2b9f5393febd9498223717037c89
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-02-06 14:27:24 -0500

    starting Week 2

---
+++ Assets/Scripts/Week2/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Week2
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager GM;
+
+        private void Awake()
+        {
+            GM = this;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _checkInput();
+        }
+
+        private void _checkInput()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
+    }
+}
+
