e773e7d3d21b889fcbb356c0402a4ab98349df0d
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-02-06 23:17:54 -0500

    added

--- Assets/Scripts/Week2/GameManager.cs
+++ Assets/Scripts/Week2/GameManager.cs
@@ -35,24 +35,33 @@ namespace Week2
             _scriptTexts = TextScript.text.Split('\n');
         }
 
-        // Update is called once per frame
+        /// <summary>
+        /// Update this instance.
+        /// </summary>
         void Update()
         {
             _checkInput();
         }
 
         /// <summary>
-        /// Checks the input.
+        /// Checks the input during Game Start Phase
         /// </summary>
         private void _checkInput()
         {
             if (GameState != State.Start) return;
             if (Input.GetMouseButtonDown(0) && !_as.isPlaying)
             {
+                _progressText();
+            }
+        }
+
+        private void _progressAudio()
+        {
+            if (!_as.isPlaying)
+            {
                 _as.clip = ChipEatingClips[_clipPointer];
                 _as.Play();
                 _clipPointer = (_clipPointer + 1) % ChipEatingClips.Length;
-                _progressText();
             }
         }
 
@@ -63,11 +72,24 @@ namespace Week2
         {
             if (_scriptPointer < _scriptTexts.Length)
             {
-                ScripterText.text = _scriptTexts[_scriptPointer++];
+                // If script text is "", then skip text, just progress audio
+                // If script text starts with ";", skip audio
+                // Otherwise progress both
+                string st = _scriptTexts[_scriptPointer++];
+                if (st.Length == 0 || st[0] != ';')
+                {
+                    _progressAudio();
+                }
+                else
+                {
+                    st = st.Substring(1);
+                }
+                ScripterText.text = st;
             }
             else
             {
                 ScripterText.text = "Game End";
+                _progressAudio();
             }
         }
     }
