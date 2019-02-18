f48e41ae10a07fde9ce6d82dbcb2a02005e8ebf9
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-02-18 18:35:19 -0500

    added music addon

---
+++ Assets/Scripts/Week3/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Week3
+{
+	public class AudioManager : MonoBehaviour
+	{
+		public static AudioManager AM;
+		public float DelayedTime = 5f;
+
+		private AudioSource _audioSource;
+
+		private void Awake()
+		{
+			AM = this;
+			_audioSource = GetComponent<AudioSource>();
+			_audioSource.PlayDelayed(DelayedTime);
+		}
+	}
+}
+
---
+++ Assets/Scripts/Week3/EventSubscriber.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using SonicBloom.Koreo;
+
+namespace Week3
+{
+	public class EventSubscriber : MonoBehaviour
+	{
+		// Start is called before the first frame update
+		void Start()
+		{
+			Koreographer.Instance.RegisterForEvents("TestEventID", FireEventDebugLog);
+		}
+
+		private void FireEventDebugLog(KoreographyEvent koreoEvent)
+		{
+			Debug.Log(Time.timeSinceLevelLoad);
+		}
+	}
+}
+
