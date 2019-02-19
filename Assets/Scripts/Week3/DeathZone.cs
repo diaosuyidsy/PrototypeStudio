e109a863268febaa49df9059c11cba62b677c77a
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-02-18 23:28:32 -0500

    added characters

---
+++ Assets/Scripts/Week3/DeathZone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Week3
+{
+	public class DeathZone : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			Destroy(collision.gameObject);
+		}
+	}
+}
+
---
+++ Assets/Scripts/Week3/OtherNPCController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using SonicBloom.Koreo;
+
+namespace Week3
+{
+	public class OtherNPCController : MonoBehaviour
+	{
+		public void OnGameStart()
+		{
+			Koreographer.Instance.RegisterForEvents("TestEventID", SpeakForFreedom);
+		}
+
+		private void SpeakForFreedom(KoreographyEvent koreoEvent)
+		{
+
+		}
+	}
+
+}
