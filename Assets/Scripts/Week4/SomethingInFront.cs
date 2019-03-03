8167f07ebafa4c66d28fa32f2a6065065b7bd871
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-03-03 17:50:46 -0500

    workding

---
+++ Assets/Scripts/Week4/SomethingInFront.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class SomethingInFront : Conditional
+{
+	public LayerMask objectLayerMask;
+	public SharedString targetTag;
+	public SharedFloat viewDistance = 50;
+
+	public override TaskStatus OnUpdate()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit, viewDistance.Value, objectLayerMask))
+		{
+			if (hit.collider.CompareTag(targetTag.Value)) return TaskStatus.Success;
+		}
+		return TaskStatus.Failure;
+	}
+}
