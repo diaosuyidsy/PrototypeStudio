89a3faeba802c2ae38acfd46f6b38a0b58f14631
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-03-31 00:08:13 -0400

    added Week 7 prototype

---
+++ Assets/Scripts/Week7/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Week7
+{
+	public class CameraMovement : MonoBehaviour
+	{
+		public GameObject Target;
+
+		private float YOffset;
+		private float ZOffset;
+		private Vector3 velocity;
+		private void Awake()
+		{
+			YOffset = transform.position.y - Target.transform.position.y;
+			ZOffset = transform.position.z - Target.transform.position.z;
+		}
+
+		private void Update()
+		{
+			var pos = transform.position;
+			pos.y = Target.transform.position.y + YOffset;
+			pos.z = Target.transform.position.z + ZOffset;
+
+			////transform.position = Vector3.Slerp(transform.position, pos, Time.deltaTime * 5f);
+			//transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, 1f);
+			transform.position = pos;
+		}
+	}
+}
+
---
+++ Assets/Scripts/Week7/PlayerControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Week7
+{
+	public class PlayerControl : MonoBehaviour
+	{
+		public float ForceImpulse = 10f;
+		public float ExplosionRange = 0.5f;
+		public float UpwardMultiplier = 0.2f;
+		public LayerMask PlayerMask;
+		public LayerMask RagdollNoCollisionMask;
+		public GameObject PenguinRagdoll;
+		public GameObject Penguin;
+		// Update is called once per frame
+		void Update()
+		{
+			CheckMouseInput();
+			if (Input.GetKeyDown(KeyCode.R))
+			{
+				SceneManager.LoadScene("Week7");
+			}
+		}
+
+		private void CheckMouseInput()
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				var v3 = Input.mousePosition;
+				v3.z = 4.5f;
+				var pos = Camera.main.ScreenToWorldPoint(v3);
+				pos.x = 0f;
+
+				Collider[] hits = Physics.OverlapSphere(pos, 0.2f, PlayerMask);
+				if (hits.Length == 0) return;
+
+				PenguinRagdoll.transform.position = Penguin.transform.position;
+				Penguin.SetActive(false);
+				PenguinRagdoll.SetActive(true);
+
+				hits = Physics.OverlapSphere(pos, 0.2f, RagdollNoCollisionMask);
+				foreach (Collider hit in hits)
+				{
+					hit.gameObject.GetComponent<Rigidbody>().AddExplosionForce(ForceImpulse, pos, ExplosionRange, UpwardMultiplier, ForceMode.Impulse);
+				}
+			}
+		}
+	}
+}
+
